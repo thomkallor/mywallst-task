cfb36e2777fdc7284b5985b7532972f325d6e613
thomkallor <gk22.ganesh@gmail.com>
2020-12-11 21:50:15 +0000

    cleanup model

--- stripeapp/subscription/models.py
+++ stripeapp/subscription/models.py
@@ -1,45 +1,12 @@
 from django.db import models
-
-# from django.contrib.auth.models import User
-
 from django.contrib.auth.models import AbstractUser
-
-from django.contrib.auth.base_user import BaseUserManager
+from django.contrib.auth.models import UserManager
 from django.utils.translation import ugettext_lazy as _
-
-
-class CustomUserManager(BaseUserManager):
-
-    def create_user(self, username, password, **extra_fields):
-        """
-        Create and save a User with the given username and password.
-        """
-        if not username:
-            raise ValueError(_('The username must be set'))
-        username = username.strip().lower()
-        user = self.model(username=username, **extra_fields)
-        user.set_password(password)
-        user.save()
-        return user
-
-    def create_superuser(self, username, password, **extra_fields):
-        """
-        Create and save a SuperUser with the given username and password.
-        """
-        extra_fields.setdefault('is_staff', True)
-        extra_fields.setdefault('is_superuser', True)
-        extra_fields.setdefault('is_active', True)
-
-        if extra_fields.get('is_staff') is not True:
-            raise ValueError(_('Superuser must have is_staff=True.'))
-        if extra_fields.get('is_superuser') is not True:
-            raise ValueError(_('Superuser must have is_superuser=True.'))
-        return self.create_user(username, password, **extra_fields)
 
 
 class User(AbstractUser):
 
-    objects = CustomUserManager()
+    objects = UserManager()
 
     stripe_id = models.CharField(max_length=255, unique=True, null=True)
 
